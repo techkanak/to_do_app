4c748a70c1f21888a1efe23cc030aa9ee22b74ed
kanak <kanakkshk@gmail.com>
2021-06-25 18:57:32 +0530

    change 2

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'To Do App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -24,8 +25,64 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List mytodo = [];
+  String toDoInput = "";
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My To DO'),
+      ),
+      body: ListView.builder(
+        itemCount: mytodo.length,
+        itemBuilder: (context, index) {
+          return Dismissible(
+            key: Key(mytodo[index]),
+            child: Card(
+              child: ListTile(
+                title: Text(mytodo[index]),
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text('Add New To Do'),
+                content: TextField(
+                  onChanged: (String value) {
+                    toDoInput = value;
+                  },
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        mytodo.add(toDoInput);
+                      });
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Add'),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
